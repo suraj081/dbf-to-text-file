e8b7663e3fd1a6ddd630a6806dd4a92d8cb56c9d
suraj081 <42827827+suraj081@users.noreply.github.com>
2022-11-04 00:20:45 +0545

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ pd.set_option('display.max_colwidth', 1000)
 pd.set_option('expand_frame_repr', False)
 from simpledbf import Dbf5
 import glob
-for file in glob.glob(r"E:\rs-paper-august\data\shapefile_bharatpur\final-output-file\*.dbf"):
+for file in glob.glob(directory\*.dbf"):
 	if file.endswith('.dbf'):
 		print(file)
 		df=Dbf5(file)
 		df1 = df.to_dataframe()
-		with open(r"E:\rs-paper-august\data\shapefile_bharatpur\Namita-didi-task\%s.txt"%file.split('.')[-2].split('\\')[-1],'w+') as f1:
+		with open("output-directory\name of file.txt",'w+') as f1:
 			f1.write(str(df1))
